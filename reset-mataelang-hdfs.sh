4d1cd4310b89d793cab5d4cdf64b02bddaf4d0d8
Ubuntu <ubuntu@ip-172-16-2-125.ap-northeast-1.compute.internal>
2022-01-06 00:42:34 +0700

    Add reset-mataelang-hdfs.sh

---
+++ reset-mataelang-hdfs.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+HADOOP_URL_PREFIX=hdfs://localhost:9000/user/${USER}
+
+#Reconstruct Hadoop-HDFS directories
+
+#(1)Move directory to HOME
+cd "${HOME}"
+
+#(2)Ask to deleate and reconstruct files
+read -p "Do you agree to delete and recreate the HDFS directories? (y/n)" ans
+case "$ans" in
+  [Yy])
+    echo "Yes"
+    #Delete directories
+    hdfs dfs -rm -r ${HADOOP_URL_PREFIX}/job
+    hdfs dfs -rm -r ${HADOOP_URL_PREFIX}/kaspa
+    hdfs dfs -rm -r ${HADOOP_URL_PREFIX}/kafka-checkpoint
+    hdfs dfs -rm -r ${HADOOP_URL_PREFIX}/kaspa-checkpoint
+    hdfs dfs -rm -r ${HADOOP_URL_PREFIX}/schema/raw_kaspa
+    #Reconstruct directries
+    hdfs dfs -mkdir -p ${HADOOP_URL_PREFIX}/job
+    hdfs dfs -mkdir -p ${HADOOP_URL_PREFIX}/kaspa
+    hdfs dfs -mkdir -p ${HADOOP_URL_PREFIX}/kafka-checkpoint
+    hdfs dfs -mkdir -p ${HADOOP_URL_PREFIX}/kaspa-checkpoint
+    hdfs dfs -mkdir -p ${HADOOP_URL_PREFIX}/schema/raw_kaspa
+    ;;
+  [Nn])
+    echo "NO"
+    echo The process is canceled.
+    ;;
+  *)
+    echo "undefined";;
+esac
+
+#(3) move directory
+cd "${OLDPWD}"
